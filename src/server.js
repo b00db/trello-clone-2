1f22da3795cb5d7c177bcb02eba6f9642b1d44c7
b00db <48580026+b00db@users.noreply.github.com>
2022-01-21 14:14:06 +0900

    Use Webpack For babel and scss

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use("/", trelloRouter);
+app.use("/static", express.static("assets"));
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
